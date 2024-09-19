fab017ea28924a8f9367c67018a5a5329230325b
@sachin.kaushik1 <sachin.kaushik@prismberry.com>
2024-09-19 10:25:35 +0530

    make arrow funtion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from 'react'
+
+const Home = () => {
   return (
     <h1>
       Hlw, Welcome to E-Book Lib.
     </h1>
-  );
+  )
 }
+
+export default Home
