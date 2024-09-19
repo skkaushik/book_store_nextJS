63a9e39f318cf4245708ccc3187ddc5d317fb231
@sachin.kaushik1 <sachin.kaushik@prismberry.com>
2024-09-19 10:16:06 +0530

    Welcome Message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <h1>
-      Hlw, Welcome in Sachin Book Store.
+      Hlw, Welcome to E-Book Lib.
     </h1>
   );
 }
